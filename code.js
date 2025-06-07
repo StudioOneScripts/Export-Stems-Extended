9cdb5168ac04c31210a14e7fe6a0da6fa25e31ef
StudioOneScripts <StudioOneScripts@users.noreply.github.com>
2025-06-07 06:23:27 -0400

    first commit

---
+++ code.js
@@ -0,0 +1,96 @@
+﻿// ------------------------------------------------------------------
+// Export Stems+ 
+//
+// This script adds a missing feature to Studio One© by temporarily
+// prefixing channel names during stem export, preserving the song’s
+// channel order in the exported files. The prefixes help maintain 
+// correct track order when importing into other DAWs, and are 
+// automatically removed afterward to leave the session unchanged.
+//
+//
+// Created by: Lawrence Farr
+// ------------------------------------------------------------------
+
+function exportStemsPlus()
+{
+	this.interfaces =  [Host.Interfaces.IEditTask]
+
+	// get the main channel list
+	let channels = Host.Objects.getObjectByUrl
+	("object://hostapp/Studio/ActiveEnvironment/MixerConsole")
+	.getChannelList (1)
+
+	// --------------------------------------
+
+	this.prepareEdit = function (context)
+	{
+		return Host.Results.kResultOk;
+	}
+
+	// --------------------------------------
+
+	this.performEdit = function (context)
+	{
+		// prefix the mixer channels
+		this.prefixMixerChannels();
+
+		// open the Export Stems dialog which will 
+		// also pause the script until it closes
+		Host.GUI.Commands.interpretCommand("Song","Export Stems")
+
+		// remove the channel prefixes
+		this.removeChannelPrefixes();
+
+		return Host.Results.kResultOk;
+	}
+
+	// -------------------------------------- 
+
+	this.prefixMixerChannels = function ()
+	{
+		// iterate the channel list
+		for (i=0; i < channels.numChannels; i++ )
+		{
+			// get the next mixer channel
+			let channel = channels.getChannel(i);
+	
+			// skip the hidden chord track channel
+			// and also skio MusicTrack channels
+			if (channel.label == "Chord Track" || 
+				channel.channelType == "MusicTrack")
+				continue;
+       
+			// prefix the channel name
+			let prefix = (i).toString().padStart(3, '0');
+			let newName = prefix + "-" + channel.label;
+			channel.label = newName;
+
+			Host.Console.writeLine(channel.label)
+		}
+	}
+
+	this.removeChannelPrefixes = function ()
+	{
+		// iterate the channel listg
+		for (i=0; i < channels.numChannels; i++ )
+		{
+			// get the next channel
+			let channel = channels.getChannel(i)
+
+			// split the label
+			var name = channel.label.split('-');
+
+			// rename the channel with the array item
+			// while trimming away any leftover spaces
+			channel.label = name[name.length-1].trim();
+		}
+	}
+}
+
+// ---------------------------------------------------------------------
+
+// entry function
+function createInstance()
+{
+	return new exportStemsPlus();
+}
