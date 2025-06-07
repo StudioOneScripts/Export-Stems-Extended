73b41f2325848cec303ae98c48e1987c055f71c0
StudioOneScripts <StudioOneScripts@users.noreply.github.com>
2025-06-07 07:07:35 -0400

    Updated comments

--- code.js
+++ code.js
@@ -1,15 +1,20 @@
 ﻿// ------------------------------------------------------------------
-// Export Stems Extended
-//
-// This script adds a missing feature to PreSonus Studio One (c) by 
-// temporarily prefixing channel names during stem export, preserving
-// the song’s channel order in the exported files. This also maintains 
-// correct track order when importing the files into other DAWs. The 
-// prefixes are automatically removed after the stem export to leave the 
-// session unchanged.
-//
-// Created by: Lawrence F.
+//                       EXPORT STEMS EXTENDED
 // ------------------------------------------------------------------
+// This script adds a missing feature to PreSonus Studio One (c) by 
+// temporarily adding numerical prefixes to channel names during stem
+// export, preserving the song’s channel order in the exported files. 
+// 
+// This also maintains correct track order when importing the files 
+// into other workstations. The prefixes are automatically removed 
+// after the stem export, leaving the session unchanged.
+//
+// Created by: Lawrence Farr June 2025
+//
+// ---------------------- Modifications? ----------------------------
+// If you modify this code and share it, please leave the original 
+// credits above and document any modifications here.
+// ------------------------------------------------------------------ 
 
 function exportStemsPlus()
 {
@@ -64,7 +69,6 @@ function exportStemsPlus()
 			let prefix = (i).toString().padStart(3, '0');
 			let newName = prefix + "-" + channel.label;
 			channel.label = newName;
-
 		}
 	}
 
